29c3d88b7d333f24a09f63f040ca474d73cd22f5
mmosnsnn <89577578+mmosnsnn@users.noreply.github.com>
2023-07-19 14:27:05 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,82 @@
+const plugins = require("../lib/plugins");
+
+const {
+    command,
+    isPrivate,
+    clockString
+} = require("../lib");
+const {
+    OWNER_NAME,
+    BOT_NAME
+} = require("../config");
+const {
+    tiny
+} = require("@viper-x/fancytext")
+const { hostname, uptime } = require("os");
+command(
+    {
+        pattern: "menu",
+        fromMe: isPrivate,
+        desc: "Show All commands",
+        dontAddCommandList: true,
+        type: "user",
+    },
+    async (message, match) => {
+        if (match) {
+            for (let i of plugins.commands) {
+                if (i.pattern.test(message.prefix + match))
+                    message.reply(tiny(
+                    `Command : ${message.prefix}${match.trim()}
+                    Description : ${i.desc}`
+                ));
+            }
+        } else {
+            let {
+                prefix
+            } = message;
+            let [date,
+                time] = new Date()
+            .toLocaleString("en-IN", {
+                timeZone: "Asia/Kolkata"
+            })
+            .split(",");
+            let menu =           `pari`
+            let cmnd = [];
+            let cmd;
+            let category = [];
+            plugins.commands.map((command, num) => {
+                if (command.pattern) {
+                    cmd = command.pattern
+                    .toString()
+                    .match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2];
+                }
+
+                if (!command.dontAddCommandList && cmd !== undefined) {
+                    let type;
+                    if (!command.type) {
+                        type = "misc";
+                    } else {
+                        type = command.type.toLowerCase();
+                    }
+                    cmnd.push({
+                        cmd, type: type
+                    });
+                    if (!category.includes(type)) category.push(type);
+                }
+            });
+            cmnd.sort();
+            category.sort().forEach((cmmd) => {
+                let comad = cmnd.filter(({
+                    type
+                }) => type == cmmd);
+                comad.forEach(({
+                    cmd
+                }, num) => {
+                    menu += `${cmd.trim()}\n`
+                });
+            });
+           menu +=                        `                                                 ${config.caption}`;
+            return await message.sendMessage(tiny(menu))
+        }
+    }
+);
