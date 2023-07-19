d78f205e3a93fd37172b7d4645d95f0bbb70d33c
mmosnsnn <89577578+mmosnsnn@users.noreply.github.com>
2023-07-19 13:42:31 +0530

    Create converter.js

---
+++ plugins/converter.js
@@ -0,0 +1,34 @@
+const {
+    command,
+    isPrivate
+} = require("../lib/")
+const googleTTS = require("google-tts-api");
+
+command(
+    {
+        pattern: "tts",
+        fromMe: isPrivate,
+        desc: "text to speech.",
+        category: "downloader",
+    },
+    async (message, match) => {
+        if (!match) {
+            message.reply('Where is the text?')
+        } else {
+            const audio = googleTTS.getAudioUrl(`${match}`, {
+                lang: "en",
+                slow: false,
+                host: "https://translate.google.com",
+            })
+            message.client.sendMessage(message.jid, {
+                audio: {
+                    url: audio,
+                },
+                mimetype: 'audio/mp4',
+                ptt: false,
+                fileName: `${match}.mp3`,
+            }, {
+                quoted: m,
+            })
+        }
+    });
