c2402dc88ae94932a14826186c002a13edc0c282
mmosnsnn <89577578+mmosnsnn@users.noreply.github.com>
2023-07-28 17:48:03 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -3,7 +3,7 @@ const {
     isPrivate
 } = require("../lib/")
 const googleTTS = require("google-tts-api");
-
+const { toAudio } = require("../lib/media");
 command(
     {
         pattern: "tts",
@@ -32,3 +32,33 @@ command(
             })
         }
     });
+command(
+  {
+    pattern: "photo",
+    fromMe: isPrivate,
+    desc: "Changes sticker to Photo",
+    type: "converter",
+  },
+  async (message, match, m) => {
+    if (!message.reply_message)
+      return await message.reply("_Reply to a sticker_");
+    if (message.reply_message.mtype !== "stickerMessage")
+      return await message.reply("_Not a sticker_");
+    let buff = await m.quoted.download();
+    return await message.sendMessage(buff, {}, "image");
+  }
+);
+command(
+  {
+    pattern: "mp3",
+    fromMe: isPrivate,
+    desc: "converts video/voice to mp3",
+    type: "downloader",
+  },
+  async (message, match, m) => {
+    //if(message.reply_message.text) return await message.reply('_Enter Video Name_')
+    let buff = await m.quoted.download();
+    buff = await toAudio(buff, "mp3");
+    return await message.sendMessage(buff, { mimetype: "audio/mpeg" }, "audio");
+  }
+);
